962625b7c408070d7b811d5221af2241a01681e9
Ariel Marcelo Diaz <ariel@infrasoft.com.ar>
2014-09-23 15:40:14 -0300

    armando cosas para imprimir

---
+++ src/sistemaliquidaciondehaberes/ImprimirPdf.java
@@ -0,0 +1,20 @@
+/**************************************
+Autor: Ariel Marcelo Diaz
+ *Sitio Web: http://www.infrasoft.com.ar 
+Desarrollo de sistemas a medidas
+ ****************************************/
+package sistemaliquidaciondehaberes;
+
+//librerias
+ import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.*;
+import java.io.FileOutputStream;
+
+
+/**
+ *
+ * Ariel Marcelo Diaz
+ */
+public class ImprimirPdf {
+
+}
